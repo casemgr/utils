422091fa68c91a4516a8a593e341812862e3e556
Mark J Stang <markjstang@gmail.com>
2017-04-30 13:23:28 -0600

    adding hypens

--- build_all.sh
+++ build_all.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
-echo "utils"
+echo "-----utils"
 cd utils
 lein clean
 lein cljsbuild once
 lein install
 cd ..
-echo "channel-monitor"
+echo "-----channel-monitor"
 cd channel-monitor  
 lein clean
 lein cljsbuild once
 lein install
 cd ..
-echo "are_you_sure"
+echo "-----are_you_sure"
 cd are_you_sure  
 lein clean
 lein cljsbuild once
 lein install
 cd ..
-echo "date_scanner"
+echo "-----date_scanner"
 cd date_scanner  
 lein clean
 lein cljsbuild once
 lein install
 cd ..
-echo "pathdb"
+echo "-----pathdb"
 cd pathdb
 lein clean
 lein cljsbuild once
